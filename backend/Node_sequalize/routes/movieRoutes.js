11057777c10ceb0fecb4a3b419a75461740cf89e
Pambali <91451814+Pambali@users.noreply.github.com>
2021-12-10 14:27:20 +0530

    Delete backend/Node_sequalize directory

--- backend/Node_sequalize/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-const {addMovie,getMovieDeatils,MovieLimitedList,MoviesByGenre}=require("../controllers/movieController")
-
-const movierouter = require('express').Router()
-
-movierouter.post("/addNewMovie",addMovie)
-movierouter.get("/findByGenre/",MoviesByGenre)
-movierouter.get("/findById/",getMovieDeatils)
-movierouter.get("/List",MovieLimitedList) 
-module.exports=movierouter;
--- backend/Node_sequalize/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const {moviePayment}=require("../controllers/paymentController");
-
-const payrouter=require("express").Router();
-
-payrouter.post("/payment",moviePayment)
-
-module.exports=payrouter
